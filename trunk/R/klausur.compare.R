8be347d54742f29b83a6d7a82890ebe47205ca1b
meik <meik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-31 22:46:50 +0000

    almost complete rewrite of klausur.compare()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/klausur@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/klausur.compare.R
+++ trunk/R/klausur.compare.R
@@ -38,95 +38,81 @@
 
 klausur.compare <- function(set1, set2, select=NA, new.set=NA){
 
-  ## a function to get only the interesting parts
-  find.diffs <- function(varname, set1=set1.sort, set2=set2.sort, n=1){
-    if(n < 1)
-      stop(simpleError("Illegal value: n < 1")) else{}
-
-    # get the row where differences occur
-    row.diff <- which(set1[[varname]] != set2[[varname]])
-    if(length(row.diff) < n)
-      return() else{}
-
-    # what values are present in both sets?
-    value1 <- set1[row.diff[n],varname]
-    value2 <- set2[row.diff[n],varname]
-
-    # find LfdNr and full name to that case
-    lfdnr <- set1[row.diff[n],"No"]
-    name <- paste(set1[row.diff[n],"FirstName"],set1[row.diff[n],"Name"])
-
-    result <- data.frame(No=lfdnr, Name=name, Item=varname, Set1=value1, Set2=value2, stringsAsFactors=FALSE)
-
-    # in case of more than one difference per row, we'll call this function recursively
-    if(length(row.diff) == n)
-      return(result)
-    else {
-      result <- rbind(result, find.diffs(varname,set1,set2,n+1))
-      return(result)
-    }
-  } ## end of function find.diffs()
-
-  # check for equality of elements
-  if(sum(names(set1) != names(set2)) > 0)
-    stop(simpleError("The objects do not include elements of the same name, cannot compare."))
-
-  if(dim(set1)[1] != dim(set2)[1])
-    stop(simpleError("The objects differ in the number of oservations, cannot compare."))
-
   # get the names of the given sets, to better understand the outcome later
   set1.name <- deparse(substitute(set1))
   set2.name <- deparse(substitute(set2))
 
-  # first create subsets, if necessary
-  if(!is.na(select[1])){
-    set1 <- subset(set1, select=select)
-    set2 <- subset(set2, select=select)
-  }
+  # first thing, if both sets are indeed the same, we can quit immediately
+  if(identical(set1, set2)) {
+    return(cat("\nThe compared objects (",set1.name," & ",set2.name,") are identical!\n", sep=""))
+  } else {
+    # before any values are even compared, check for equality of elements
+    if(!identical(names(set1), names(set2)))
+      stop(simpleError("The objects do not include elements of the same name, cannot compare."))
+    if(!identical(dim(set1)[1], dim(set2)[1]))
+      stop(simpleError("The objects differ in the number of oservations, cannot compare."))
 
-  # replace NAs, they're always trouble, and we only care for equality, not values
-  set1[is.na(set1)] <- "--"
-  set2[is.na(set2)] <- "--"
+    ## prepare sets for comparison
+    # first create subsets, if necessary
+    if(!is.na(select[1])){
+      set1 <- subset(set1, select=select)
+      set2 <- subset(set2, select=select)
+      subset.varname <- paste(names(set1), collapse=", ")
+      warning(paste("Only a subset was compared:\n", subset.varname, sep=""), call.=FALSE)
+      # quit if the subset is identical
+      if(identical(set1, set2))
+	return(cat("\nThe compared objects (",set1.name," & ",set2.name,") are identical!\n\n", sep=""))
+    } else{}
+    # replace NAs, they're always trouble, and we only care for equality, not particular values
+    set1[is.na(set1)] <- "--"
+    set2[is.na(set2)] <- "--"
+    # then sort the data according to LfdNr
+    set1 <- set1[order(set1$No),]
+    set2 <- set2[order(set2$No),]
+    ## done with preparations here
 
-  # then sort the data according to LfdNr
-  set1.sort <- set1[order(set1$No),]
-  set2.sort <- set2[order(set2$No),]
+    ## the actual comparison
+    # create an array with indices of differences
+    set.diff.array <- which(set1 != set2, arr.ind=TRUE)
+    # total number of differences, needed for the sapply() call below
+    uneq <- dim(set.diff.array)[1]
+    # results are sorted by column, but we want them sorted by row
+    set.diff.array <- set.diff.array[order(set.diff.array[,1]),]
 
-  # compare set1 and set2 and get TRUE/FALSE matrix
-  comp.sets <- (set1.sort == set2.sort)
+    # the main act!
+    differences <- t(
+      sapply(1:uneq,
+	function(x){
+	  if(uneq > 1) {
+	    idx <- set.diff.array[x,]
+	  } else {
+	    idx <- set.diff.array
+	  }
+	  varname <- names(set1)[idx[2]]
+	  # what values are present in both sets?
+	  value1 <- set1[idx[1],idx[2]]
+	  value2 <- set2[idx[1],idx[2]]
+	  # find LfdNr and full name to that case
+	  lfdnr <- set1[idx[1],"No"]
+	  name <- paste(set1[idx[1],"FirstName"],set1[idx[1],"Name"])
+	  result <- data.frame(No=lfdnr, Name=name, Item=varname, Set1=value1, Set2=value2, stringsAsFactors=FALSE)
+	  return(result)
+	} # end function(x)
+      ) # end sapply()
+    ) # end t()
 
-  # get vector with variable names
-  var.names <- colnames(comp.sets)
-
-  # see if there are any differences
-  if(sum(!comp.sets) > 0){
-    # first, find out which variables show differences
-    vars.diff.index <- which(as.logical(rowSums(t(!comp.sets)) > 0))
-    vars.diff.names <- names(set1.sort)[vars.diff.index]
-    # now, find out which observations, calling the function defined above
-    differences <- data.frame(No=c(), Name=c(), Item=c(), Set1=c(), Set2=c(), stringsAsFactors=FALSE)
-    for(var.name in vars.diff.names){
-      item.diffs <- find.diffs(var.name)
-      if(is.null(dim(item.diffs)))
-	differences <- rbind(differences,t(data.frame(item.diffs)))
-      else{
-	differences <- rbind(differences,item.diffs)
-      }
-    }
-    # set names to the input set names
+    # set the names of the given sets, to better understand the outcome
     colnames(differences)[4] <- set1.name
     colnames(differences)[5] <- set2.name
     rownames(differences) <- c(1:dim(differences)[1])
 
     # check if a new set should be created, with dubious values replaced by NA
     if(!is.na(new.set)){
-      save.set.exp <- paste(new.set, " <<- set1.sort ; ",new.set,"[comp.sets == FALSE] <<- NA", sep="")
+      save.set.exp <- paste(new.set, " <<- set1 ; ",new.set,"[set1 != set2] <<- NA", sep="")
       eval(parse(text = save.set.exp))
     } else{}
-      
-    cat("The objects differ:\n\n")
+
+    cat("\nThe objects differ:\n\n")
     return(differences)
   }
-  else
-    return(cat("The compared objects (",set1.name," & ",set2.name,") are identical!\n", sep=""))
 }
