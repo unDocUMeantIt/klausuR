85cc2a69874bc25a452141c54d2ad71ed834b08a
meik <meik@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-05 17:55:26 +0000

    devel release 0.12-9 (on reaktanz.de)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/klausur@94 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/klausuR/R/klausuR-package.R
+++ trunk/klausuR/R/klausuR-package.R
@@ -4,7 +4,7 @@
 #' Package: \tab klausuR\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.12-9\cr
-#' Date: \tab 2013-08-02\cr
+#' Date: \tab 2013-08-05\cr
 #' Depends: \tab R (>= 2.9.0),xtable,psychometric,methods,graphics,tools\cr
 #' Enhances: \tab rkward\cr
 #' Encoding: \tab UTF-8\cr
