124489abf51b5dac4ddb42271ef03126e2ec1a63
unDocUMeantIt <m@reaktanz.de>
2018-07-29 23:21:23 +0200

    released as 0.12-13

--- R/klausuR-package.R
+++ R/klausuR-package.R
@@ -1,11 +1,12 @@
-#' Multiple Choice Test Evaluation.
+#' \packageDescription{klausuR}
 #'
+#' The DESCRIPTION file:
 #' \tabular{ll}{
 #' Package: \tab klausuR\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.12-12\cr
-#' Date: \tab 2017-03-01\cr
-#' Depends: \tab R (>= 2.9.0), xtable, psychometric, polycor, methods, graphics, tools\cr
+#' Date: \tab 2018-07-29\cr
+#' Depends: \tab R (>= 2.9.0), xtable, psychometric, polycor\cr
 #' Enhances: \tab rkward\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>= 3)\cr
@@ -13,20 +14,10 @@
 #' URL: \tab https://reaktanz.de/?c=hacking&s=klausuR\cr
 #' }
 #'
-#' A set of functions designed to quickly generate results of a multiple choice test.
-#' Generates detailed global results, lists for anonymous feedback and personalised result feedback
-#' (in LaTeX and/or PDF format), as well as item statistics like Cronbach's alpha or
-#' disciminatory power. 'klausuR' also includes a plugin for the R GUI and IDE RKWard, providing
-#' graphical dialogs for its basic features. The respective R package 'rkward' cannot be
-#' installed directly from a repository, as it is a part of RKWard. To make full use of this
-#' feature, please install RKWard from https://rkward.kde.org (plugins are detected automatically).
-#' Due to some restrictions on CRAN, the full package sources are only available from the
-#' project homepage.
+#' @title
+#' \packageTitle{klausuR}
+#' @author
+#' \packageAuthor{klausuR}
 #'
-#' @aliases klausuR-package
-#' @name klausuR-package
-#' @docType package
-#' @title The klausuR Package
-#' @author m.eik michalke
-#' @keywords package
-NULL
+#' Maintainer: \packageMaintainer{klausuR}
+"_PACKAGE"
